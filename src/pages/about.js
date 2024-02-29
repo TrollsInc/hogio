b1d77beb6c8e0894f56dba263374689458941a34
Ryan Zhang <rzhang607@gatech.edu>
2024-02-29 11:38:56 -0500

    About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,11 @@ function About() {
                 Website created by: <br/>
                 Ryan Z, Sean Y
             </h1>
+            <h2>
+                algoVis is created for visualizing algorithms in Computer Science and other adjacent
+                fields to improve students' understanding and make it accessible to a broader range
+                of people.
+            </h2>
         </div>
     );
 }
